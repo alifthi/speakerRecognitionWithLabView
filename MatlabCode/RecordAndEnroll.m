25a11919a4a249335c68289e1ae7849ab64c4889
alifthi <alifathi8008@gmail.com>
2023-05-05 20:13:57 +0430

    speakerRecognitionWithLabView: enroll new record

--- MatlabCode/RecordAndEnroll.m
+++ MatlabCode/RecordAndEnroll.m
@@ -1,8 +1,9 @@
 %% readAudio
-t = 6;
+t = 3;
 f = 16+3;
 rec = audiorecorder(f,8,1);
 record(rec,t);
+disp('recording...');
 tic;
 while toc<=(t+1)
     pause(1);
@@ -14,3 +15,22 @@ fname = 'ivec-english-16kHz.zip';
 tmpDir = '../Model/';
 zipFile = fullfile(tmpDir,fname);
 unzip(zipFile,tempdir);
+%% enroll new record
+addpath(tempdir)
+sr = speakerRecognition;
+enrollRecord = recData(1:3*fs);
+label = "Ali";
+enroll(sr,enrollRecord,label)
+%% verify record
+testRec = audiorecorder(f,8,1);
+disp('recording...');
+record(testRec,t);
+tic;
+while toc<=(t+1)
+    pause(1);
+    disp('.');
+end
+testRecord = getaudiodata(testRec);
+
+candidates = identify(sr,testRecord);
+isVerified = verify(sr,testRecord,label);
